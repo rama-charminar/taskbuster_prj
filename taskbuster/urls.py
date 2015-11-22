17d10b005e0ef3e130bdd40a11aa7cea4a50bc80
Rama Krishna <connect2ramki@gmail.com>
2015-11-23 01:15:21 +0530

    save all master pages

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import include, url
-from taskbuster.views import Rama,rama,List,index
+from taskbuster.views import index,AppMaster,EnvMaster,ServerMaster,UserMaster,login,Login,SaveAppMaster,SaveEnvMaster,SaveUserMaster,SaveServerMaster
 
 urlpatterns = [
-	url(r'^Rama/$', Rama, name='Rama'),
-	url(r'^rama/$', rama, name='rama'),
-	url(r'^List/$',List,name='List'),
+	url(r'^login/$',login,name='login'),
+	url(r'^Login/$',Login,name='Login'),
 	url(r'^index/$',index,name='index'),
-	
+	url(r'^AppMaster/$',AppMaster,name='AppMaster'),
+	url(r'^EnvMaster/$',EnvMaster,name='EnvMaster'),
+	url(r'^ServerMaster/$',ServerMaster,name='ServerMaster'),
+	url(r'^UserMaster/$',UserMaster,name='UserMaster'),
+	url(r'^SaveAppMaster/$',SaveAppMaster,name='SaveAppMaster'),
+	url(r'^SaveEnvMaster/$',SaveEnvMaster, name='SaveEnvMaster'),
+	url(r'^SaveUserMaster/$',SaveUserMaster, name='SaveUserMaster'),
+	url(r'^SaveServerMaster/$',SaveServerMaster, name='SaveServerMaster'),
 ]
